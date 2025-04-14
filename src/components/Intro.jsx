42dc5d8fbc7fcdbbd61db2032aff8813ac10efac
Helena Hope-Carter <90761872+helboi4@users.noreply.github.com>
2025-04-14 11:29:34 +0100

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,8 +5,7 @@ function intro() {
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Helena</h1>
         <p className="text-base md:text-xl mb-3 font-medium">Full stack Engineer</p>
-        <p className="text-sm max-w-xl mb-6 font-bold">I am a software engineer with one years experience. Trained in full-stack, currently working in a backend role using Java and Spring at UK Retailer Marks and Spencers. I have worked extensively with Azure devops in my job and am currently focused on expanding my devops skills. Outside of work I create both frontend and backend projects using other languages like Javascript and python. 
-        <br/>
+        <p className="text-sm max-w-xl mb-6 font-bold">I am a software engineer with three years experience. I am currently looking for work after 2 years at Universal Music Group as a full stack developer. I worked with Java, Spring, Angular, Ionic and AWS in that role. My biggest acheivement was completing an ionic app rewrite and new identity provider integration in half the time estimated by the senior frontend engineer on my team, therefore doubling productivity. I'm hoping to keep stacking up sucesses like that in my next role, and to learn some new technologies.
         Feel free to peruse my website and{' '}
         <a
         href="https://www.linkedin.com/in/helenahc/"
